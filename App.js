86e36cb2deaf5946ab4bc3a72c25106a8dc05666
Hayley Brinicombe <103518155+HayleyBrinicombe@users.noreply.github.com>
2022-10-19 13:08:15 +0100

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+      }}
+    >
+      <View>
+        <Text style={{ fontSize: 30, fontWeight: "bold", color: "#20315f" }}>
+          Max Suite
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#32a9f8",
+          padding: 20,
+          width: "90%",
+          borderRadius: 5,
+          flexDirection: "row",
+          justifyContent: "space-between"
+        }}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 18, color:'#fff' }}>Let's Begin</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#fff" />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
+
+
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
